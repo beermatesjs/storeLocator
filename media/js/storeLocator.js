9a9e4dc6c25dfdeaaef5b82d3fdabadd9c8b3d1f
Kenny Shen <kenny@northpoles.sg>
2010-12-11 17:48:23 +0800

    Link accordion and map together (via click events)

--- media/js/storeLocator.js
+++ media/js/storeLocator.js
@@ -1,5 +1,8 @@
 var stores = [];
 var settings = {};
+var markers = [];
+var infoWindows = [];
+var activeInfoWindow;
 
 function loadStores(root) {
   var feed = root.feed;
@@ -17,47 +20,48 @@ function loadSettings(root) {
 
 function initialize() {
 	var center = new google.maps.LatLng(parseFloat(settings["Starting Center Latitude"]), parseFloat(settings["Starting Center Longitude"]));
-  var mapOptions = {
-    zoom: parseInt(settings["Starting Zoom"]),
-    center: center,
-    mapTypeId: eval("google.maps.MapTypeId." + settings["Map Type"])
-  };
+    var mapOptions = {
+        zoom: parseInt(settings["Starting Zoom"]),
+        center: center,
+        mapTypeId: eval("google.maps.MapTypeId." + settings["Map Type"])
+    };
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
 	// Load markers
-	var markers = [];
-	var infoWindows = [];
 	for (var i = 0; i < stores.length; ++i) {
-    var store = stores[i];
-    writeAccordion(store);
+        var store = stores[i];
+        writeAccordion(store);
     
-    var name = store.gsx$storename.$t;
-    var address = store.gsx$address.$t;
-    var lat = parseFloat(store.gsx$latitude.$t);
-    var lng = parseFloat(store.gsx$longitude.$t);
+        var name = store.gsx$storename.$t;
+        var address = store.gsx$address.$t;
+        var lat = parseFloat(store.gsx$latitude.$t);
+        var lng = parseFloat(store.gsx$longitude.$t);
 
-    var latlng = new google.maps.LatLng(lat, lng);
-    var marker = new google.maps.Marker({
-      position: latlng,
-      map: map,
-      title: name
-    });
-    var content_str = '<p><strong>' + name + '</strong><br />' + address + '</p>';
-    var infoWindow = new google.maps.InfoWindow({
-          position: marker.position,
-          content: content_str
-    });
-    (function(marker, infoWindow) {
-        google.maps.event.addListener(marker, 'click', function() {
-            $.each(infoWindows, function(idx) {
-                infoWindows[idx].close();
-            });
-            infoWindow.open(marker.map);
+        var latlng = new google.maps.LatLng(lat, lng);
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+          title: name
         });
-    })(marker, infoWindow)
-    infoWindows.push(infoWindow);
-    markers.push(marker);
+        var content_str = '<p><strong>' + name + '</strong><br />' + address + '</p>';
+        var infoWindow = new google.maps.InfoWindow({
+              content: content_str,
+              title: name,
+              marker: marker
+        });
+        (function(marker, infoWindow, i) {
+            google.maps.event.addListener(marker, 'click', function() {
+                if (activeInfoWindow != undefined) {
+                    activeInfoWindow.close()
+                }
+                infoWindow.open(marker.map, marker);
+                activeInfoWindow = infoWindow;
+                $('#accordion').accordion("activate", i);
+            });
+        })(marker, infoWindow, i)
+        markers.push(marker);
+        infoWindows.push(infoWindow);
   }
   var markerCluster = new MarkerClusterer(map, markers, {maxZoom: parseInt(settings["Marker Cluster Zoom"])});
 }
@@ -65,14 +69,28 @@ function initialize() {
 // writes store information
 function writeAccordion(store) {
     var name = store.gsx$storename.$t;
-    var address = store.gsx$address.$t;
-    $('#accordion').append('<h3><a href="#">' + name + '</a></h3><div><p>' + address);
+    var description = store.gsx$description.$t;
+    $('#accordion').append('<h3><a href="#">' + name + '</a></h3><div><p>' + description);
 }
 
 $(document).ready(function() {
     initialize();
     $(function() {
         $("#store-list").draggable();
-        $("#accordion").accordion({ header: "h3" });
+        $("#accordion").accordion({ 
+            header: "h3",
+            change: function(events, ui) {
+                var newheader = ui.newHeader.text();
+                if (activeInfoWindow != undefined) {
+                    activeInfoWindow.close()
+                }
+                $.each(infoWindows, function(idx) {
+                    if (infoWindows[idx].title === newheader) {
+                        infoWindows[idx].open(infoWindows[idx].marker.map, infoWindows[idx].marker);
+                        activeInfoWindow = infoWindows[idx];
+                    }
+                });
+            },
+        });
     });
 });
